9325d200bc0fb49ca58217742201d35445a984fa
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-05-29 22:09:48 +0200

    Configuration de l'application Express avec une connexion MongoDB, des routes pour les livres et les utilisateurs, et un service de fichiers statiques pour les images.

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const booksRoutes = require('./routes/books');
+const userRoutes = require('./routes/user');
+const path = require('path');
+// const dataRoutes = require('./routes/data');
+
+const app = express();
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
+console.log(process.env);
+
+mongoose.connect(`mongodb+srv://${dbUsername}:${dbPassword}@clusterfirstone.zwko5fr.mongodb.net/?retryWrites=true&w=majority&appName=ClusterFirstOne`)
+    .then(() => {
+        console.log('Connexion à MongoDB réussie !');
+    })
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+// app.use('/api/data', dataRoutes);
+app.use('/api/books', booksRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
