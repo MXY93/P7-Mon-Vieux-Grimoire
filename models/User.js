6041af258755a8ef7228ae497ecbc0e716a5d5e0
Maxime Videau <147695339+MXY93@users.noreply.github.com>
2024-05-30 19:04:21 +0200

    Ajout de la validation de l'email au schéma de l'utilisateur pour garantir un format d'email correct

--- models/User.js
+++ models/User.js
@@ -2,7 +2,17 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-    email: {type: String, required: true, unique: true},
+    email: {
+        type: String, 
+        required: true, 
+        unique: true, 
+        validate: { 
+            validator: function(v) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email!`
+        }
+    },
     password: {type: String, required: true}
 });
 
